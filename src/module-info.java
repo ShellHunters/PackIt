bf395d33d6168247eb36e33c9ab9ce4eb2305c60
Nassim <aa.nassim.ddr@gmail.com>
2020-03-15 20:07:23 +0100

    module-info.java setup

--- src/module-info.java
+++ src/module-info.java
@@ -1,2 +1,6 @@
 module PackIt {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+    //requires mysql.connector.java; //ki tegla3 el comment tjik erreur lazem tkhdem el package ta3 connection 9bel :^]
 }
